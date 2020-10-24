3141815845973234eff25c6ad7dc91c1f9073e42
Stepan <kvantoreps@gmail.com>
2020-10-24 13:44:36 +0300

    make work on example

--- comp.py
+++ comp.py
@@ -21,7 +21,7 @@ def get_connected_e(V, G):
 # Количество цепей(ребер) связывающих вершину e с множеством нераспределенных вершин I
 def L1(e):
     res = 0
-    eV = get_connected_V(e, V)
+    eV = get_connected_V(e, G)
     for V in eV:
         for i in I:
             if G[i][V]:
@@ -36,7 +36,7 @@ def out_chains(T):
 
     # Находим все ребра соединенные с вершинами в T
     for e in T:
-        connected_V.union(set(get_connected_V(e, G)))
+        connected_V.update(get_connected_V(e, G))
     
     # Находим все вершины смежные с вершинами в T
     for V in connected_V:
@@ -46,7 +46,7 @@ def out_chains(T):
         for e in Ve:
             if e not in T:
                 outer_V.add(V)
-    return len(V_set)
+    return len(outer_V)
 
 def L2(T, e):
     return out_chains(T.union({e}))
@@ -56,9 +56,9 @@ def L3(T, e):
     TV = set()
     
     for i in T:
-        TV.update(get_connected_V(i))
+        TV.update(get_connected_V(i, G))
 
-    return let(TV)
+    return len(TV)
 
 
 def find_max_L1():
@@ -71,26 +71,62 @@ def find_max_L1():
             max_val = temp
     return [e for (e, val) in I1.items() if val == max_val][0]
 
-# def find_
 
-m = 3 # Ограничение на число внешних связей
+def find_outer_connections_less_m(T, I):
+    # Находим такие вершины (из нераспределенных) при присоединении которых число внешних связей будет меньше m
+    not_distributed = I.difference(T)
+    L2_each_e = [(e, L2(T, e)) for e in not_distributed]
+    
+    return [(e, eL2) for (e, eL2) in L2_each_e if eL2 <= m]
 
-r = 0 # Номер куска
+def find_best_with_max_inner_connections(T, I, filtered_e):
+    # Находим вершину (из полученных на предыдущем шаге) с максимальным количеством связей с текущим куском
+    L3_each_filtered_L2_e = [(e, eL2, L3(T, e)) for (e, eL2) in filtered_e]
+
+    max_val = 0
+    for (e, eL2, eL3) in L3_each_filtered_L2_e:
+        if max_val < eL3:
+            max_val = eL3
+
+    Iv = []
+    for (e, eL2, eL3) in L3_each_filtered_L2_e:
+        if eL3 == max_val:
+            Iv.append((e, eL2, eL3))
+
+    min_L2 = Iv[0]
+    if len(Iv) != 0:
+        for e in Iv:
+            if min_L2[1] > e[1]:
+                min_L2 = e
+
+    return min_L2[0]
+
+
+m = 5 # Ограничение на число внешних связей
+
+parts = []
 n_max = 3
 
-r += 1 # Новый кусок
-E_r = 0 # Множество вершин в куске r
-a = n_max
-index = 1
+while len(I):
+    Ee_r = set()
+    # первая вершина в куске
+    e = find_max_L1()
+    Ee_r.add(e)
 
-e = find_max_L1()
-Ee_r = set()
-Ee_r.add(e)
+    if len(I.difference(Ee_r)) == 0:
+        parts.append(Ee_r)
+        break
 
-not_distributed = I.difference(Ee_r)
-L2_each_e = [(e, L2(Ee_r, e)) for e in not_distributed]
-L2_each_e_filtered = [(e, eL2) for (e, eL2) in L2_each_e if eL2 <= m]
+    while len(Ee_r) < n_max and len(I.difference(Ee_r)) != 0:
+        # остальные
+        L2_each_e_filtered = find_outer_connections_less_m(Ee_r, I)
 
-L3_each_filtered_L2_e = [(e, eL2, L3(e)) for (e, L2) in L2_each_e_filtered]
+        if (len(L2_each_e_filtered) == 0):
+            print('Не возможно')
 
-    
+        Ee_r.add(find_best_with_max_inner_connections(Ee_r, I, L2_each_e_filtered))
+        I.difference_update(Ee_r)
+
+    parts.append(Ee_r)
+
+print(parts)
